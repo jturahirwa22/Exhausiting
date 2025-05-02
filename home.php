ed403d94f54d54e8f09a895fce43f31f72c2519a
Regis NDAMIRA <turahirwadali@gmail.com>
2025-05-02 14:36:14 +0200

    first commit

---
+++ home.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Instagram</title>
+  <link rel="stylesheet" href="style.css" />
+  <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+</head>
+<body>
+  <div class="container">
+    <div class="login-box">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2560px-Instagram_logo.svg.png" alt="Instagram" class="logo"/>
+
+      <form>
+        <input type="text" placeholder="Phone number, username, or email" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Log In</button>
+      </form>
+
+      <div class="divider">
+        <div class="line"></div>
+        <div class="or">OR</div>
+        <div class="line"></div>
+      </div>
+
+      <a href="#" class="fb-login">Log in with Facebook</a>
+      <a href="#" class="forgot">Forgot password?</a>
+    </div>
+
+    <div class="signup-box">
+      <p>Don't have an account? <a href="#">Sign up</a></p>
+    </div>
+  </div>
+</body>
+</html>
